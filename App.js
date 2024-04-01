1a76dc9124fe3c5233aed04bf0b2a1b488cfa552
Carlos Macarao <82442020+carlosmacarao@users.noreply.github.com>
2024-04-02 01:04:00 +0200

    new task

--- App.js
+++ App.js
@@ -20,6 +20,13 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1
+        }}
+        keyboardShouldPersistTaps='handled'
+      ></ScrollView>
 
       {/* Today's Task */}
       <View style={styles.tasksWrapper}>
@@ -51,6 +58,35 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
   items: {
-
-  }
+    marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {},
+  
 });
